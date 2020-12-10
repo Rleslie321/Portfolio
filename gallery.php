735643db4fa23bd0485728bed8b8d52016c0c8cf
leslier4 <leslier4@wwu.edu>
2020-12-09 19:42:16 -0800

    fixed issues with gallery pages

--- gallery.php
+++ gallery.php
@@ -5,10 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.css">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="styles/main.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="node_modules/@fortawesome/fontawesome-free/css/all.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.11.1/baguetteBox.min.css">
     <link rel="shortcut icon" type="image/ico" href="robs_icon.ico">
     <title>Robert Leslie  | Gallery</title>
 </head>
@@ -96,7 +96,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/js/bootstrap.min.js"></script>
     <script src="js/lightbox.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.11.1/baguetteBox.min.js"></script>
 
 </body>
 
--- js/carousel.js
+++ js/carousel.js
@@ -1,29 +1,16 @@
 
 
 $(document).ready(function(){
-    $(".owl-carousel").each(function(index, el) {
-        var containerHeight = $(el).height();
-        $(el).find("img").each(function(index, img) {
-            var w = $(img).prop('naturalWidth');
-            var h = $(img).prop('naturalHeight');
-            $(img).css({
-            'width': Math.round(containerHeight * w / h) + 'px',
-            'height': containerHeight + 'px'
-            });
-        }),
-        $(el).owlCarousel({
-            items: 1,
-            autoWidth: true,
-            loop: true,
-            autoplay:true,
-            autoplayTimeout:4000,
-            margin:10,
-            center: true,
-            // autoWidth: true,
-            autoplayHoverPause:true,
-            nav    : true,
-            smartSpeed :900,
-            navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
-        });
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        autoWidth: true,
+        loop: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        margin: 10,
+        center: true,
+        autoplayHoverPause: true,
+        nav: true,
+        navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
     });
 });
